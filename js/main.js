1326c4ca55750bce86f3aba3adb3ef8ab21d03f1
maria Menendez Pino <m.menendezpino@gmail.com>
2025-01-09 22:44:18 -0500

    Delete js/main.js

--- js/main.js
+++
@@ -1,6 +0,0 @@
-document.addEventListener("DOMContentLoaded", function() {
-    const fadeIns = document.querySelectorAll('.fade-in');
-    fadeIns.forEach(element => {
-        element.classList.add('visible');
-    });
-});
